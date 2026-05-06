8a10e4d64206ada84445ab8f3a0545ea96469f0d
Zubish <lordbish98@gmail.com>
2026-05-06 09:32:54 +0100

    simplify vercel api

--- api/account/withdrawals.js
+++
@@ -1,5 +0,0 @@
-const { handleRequest } = require("../../backend/server");
-
-module.exports = async function handler(request, response) {
-    return handleRequest(request, response);
-};
---
+++ api/auth.js
@@ -0,0 +1,14 @@
+const { handleRequest } = require("../backend/server");
+
+module.exports = async function handler(request, response) {
+    restorePath(request, "/api/auth");
+    return handleRequest(request, response);
+};
+
+function restorePath(request, basePath) {
+    const url = new URL(request.url, "http://blackcrow.local");
+    const path = url.searchParams.get("path");
+    if (!path) return;
+    url.searchParams.delete("path");
+    request.url = `${basePath}/${path}${url.search}`;
+}
